8041182f47c21c42a9dfd4743d445c7236eba409
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-07-24 18:56:41 +0300

    run all adapters

--- src/main/java/com/mytests/spring/springintegration5/annotattionstest0/RunAdaptersTest.java
+++ src/main/java/com/mytests/spring/springintegration5/annotattionstest0/RunAdaptersTest.java
@@ -2,23 +2,24 @@ package com.mytests.spring.springintegration5.annotattionstest0;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.integration.endpoint.SourcePollingChannelAdapter;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.PollableChannel;
 
 public class RunAdaptersTest {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(AdaptersConfig.class);
+        testAdaptersFromDiffContexts(AdaptersConfig0.class, "ica0channel");
+        testAdaptersFromDiffContexts(AdaptersConfig1.class, "ica1channel");
+        testAdaptersFromDiffContexts(AdaptersConfig2.class, "ica2channel");
 
-        PollableChannel out = ctx.getBean("channel1", PollableChannel.class );
+    }
+
+    private static void testAdaptersFromDiffContexts(Class configClass, String channel) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(configClass);
+
+        PollableChannel out = ctx.getBean(channel, PollableChannel.class );
         Message mess = out.receive(100);
         System.out.println(mess);
-        System.out.println("_________________________");
-
-        for (String beanDefinitionName : ctx.getBeanDefinitionNames()) {
-            System.out.println(beanDefinitionName);
-        }
         ((AnnotationConfigApplicationContext) ctx).close();
     }
 }
