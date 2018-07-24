97aa375c4e2917885cc087a831849c932a27a805
irina <irina@me.com>
2018-07-24 16:17:39 +0300

    test method for AdaptersConfigTest.java

--- src/test/java/com/mytests/spring/springintegration5/annotattionstest0/AdaptersConfigTest.java
+++ src/test/java/com/mytests/spring/springintegration5/annotattionstest0/AdaptersConfigTest.java
@@ -1,17 +1,15 @@
 package com.mytests.spring.springintegration5.annotattionstest0;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.endpoint.SourcePollingChannelAdapter;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.function.Supplier;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = AdaptersConfig.class)
@@ -25,11 +23,15 @@ public class AdaptersConfigTest {
 
     @Autowired
     @Qualifier("myadapter.source")
-    private Supplier<?> adapterSource;
+    private Supplier<?> messageSource;
+
+
 
     @Test
     public void name() {
+        Assert.assertNotNull(adapter);
+        Assert.assertNotNull(messageSource);
         System.out.println(adapter.getMessageSource().toString());
-        System.out.println(adapterSource.toString());
+        System.out.println(messageSource.get());
     }
 }
